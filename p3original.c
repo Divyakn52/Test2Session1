b0ce664cc63cf9857f0f8431b54088e1673051e9
Divzz <97013125+Divyakn52@users.noreply.github.com>
2022-02-24 08:52:12 +0000

    coding completed

---
+++ p3original.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int input_number()
+{
+  int a;
+  printf("enter a number\n");
+  scanf("%d",&a);
+  return a;
+}
+int is_prime(int n)
+{
+  for(i=2;i<n;i++)
+    {
+      printf("number is not prime\n");
+    }
+  else
+}
