e27dce34ad52fcc383d0b12112f73a8af13fb303
Divzz <97013125+Divyakn52@users.noreply.github.com>
2022-03-04 13:50:24 +0000

    coding completed

--- p4original.c
+++ p4original.c
@@ -2,6 +2,18 @@
 int input();
 {
   int n;
-  printf("enter the number\n")
-  scanf("%d",&n)
+  printf("enter number of fibo to find\n");
+  scanf("%d",&n);
+  return n;
+}
+int find_fibo(int n)
+{
+  int i,a=0,b=1,sum=0;
+  for(i=1;i<n;i++)
+    {
+      sum=a+b;
+      a=b
+        b=sum
+    }
+  return sum;
 }
